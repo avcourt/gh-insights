e814d39970599b0b12400d8805144ba1805a420e
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2019-11-01 13:20:25 -0400

    ✨ few quality of life tweaks
    - update README.md details on what permissions the Github api token
    requires
    - make the Github user settable from an environment variable (no
    modiying the script required)
    - move the body of the script into a function using normal `if
    __main__..` pattern
    - black format and isort imports
    - correct hashbang to work in virtualenvs

--- traffic.py
+++ traffic.py
@@ -1,36 +1,49 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import pandas
 import os
-import requests
-from requests.auth import HTTPBasicAuth
 from urllib.parse import urljoin
 
-user = 'avcourt'                # your github username
-key = os.environ.get('GH_API')  # your secret access token. you can hardcode yours.
+import pandas
+import requests
+from requests.auth import HTTPBasicAuth
 
-repos_url = f'https://api.github.com/users/{user}/repos'
-repo_names = [repo["name"] for repo in requests.get(repos_url).json()]
 
-insights = []
-base_url = f'https://api.github.com/repos/{user}/'
+def main():
+    """Main cli"""
+    user = os.environ.get("GH_USER", "avcourt")  # your github username
+    key = os.environ.get("GH_API")  # your secret access token. you can hardcode yours.
 
-print("Getting traffic insights for repos:")
-for repo in repo_names:
-    print(f"\t- github.com/{user}/{repo}/")
-    repo_url = urljoin(base_url, repo + '/')
-    traffic = requests.get(urljoin(repo_url, 'traffic/views'),
-                           auth=HTTPBasicAuth(user, key)).json()
+    repos_url = f"https://api.github.com/users/{user}/repos"
+    repo_names = [repo["name"] for repo in requests.get(repos_url).json()]
 
-    clones = requests.get(urljoin(repo_url, 'traffic/clones'),
-                        auth=HTTPBasicAuth(user, key)).json()["count"]
+    insights = []
+    base_url = f"https://api.github.com/repos/{user}/"
 
-    insights.append({'repo': repo,
-                     'views': traffic['count'],
-                     'uniques': traffic['uniques'],
-                     'clones': clones
-                     })
+    print("Getting traffic insights for repos:")
+    for repo in repo_names:
+        print(f"\t- github.com/{user}/{repo}/")
+        repo_url = urljoin(base_url, repo + "/")
+        traffic = requests.get(
+            urljoin(repo_url, "traffic/views"), auth=HTTPBasicAuth(user, key)
+        ).json()
 
-print("\n-- INSIGHTS --------------- Views / Clones --")
-print("---------------------------------------------")
-print(pandas.DataFrame(insights).to_string(index=False))
+        clones = requests.get(
+            urljoin(repo_url, "traffic/clones"), auth=HTTPBasicAuth(user, key)
+        ).json()["count"]
+
+        insights.append(
+            {
+                "repo": repo,
+                "views": traffic["count"],
+                "uniques": traffic["uniques"],
+                "clones": clones,
+            }
+        )
+
+    print("\n-- INSIGHTS --------------- Views / Clones --")
+    print("---------------------------------------------")
+    print(pandas.DataFrame(insights).to_string(index=False))
+
+
+if __name__ == "__main__":
+    main()
